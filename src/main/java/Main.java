fcc95b10737c2b31744cfe16a93907e40dc05685
Rajat Saxena <rajatsaxena93@gmail.com>
2019-02-23 13:45:12 +0530

    Code skeleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,55 @@
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.*;
+import scala.Tuple2;
 
 public class Main
 {
+    private static Dataset<Row> dataDS;
+    private static Dataset<Row> tempDS;
+    private static Dataset<Row> anonymizedDS;
+    private static Dataset<Tuple2<String, Long>> resultDS;
+
     public static void main(String args[])
     {
-
         SparkSession spark = SparkSession
                 .builder()
                 .appName("PII Anonymization")
                 .master("local")
                 .getOrCreate();
 
-        Dataset<Row> reader =  spark.read()
+        dataDS = spark.read()
                 .format("csv")
                 .option("header", "true")
                 .load(" ");
+        // TODO: Update the above
+
+        String deleteCol = "deleteCol";
+        String rowIndex = "rowIndex";
+        String columnName = "desc"; // TODO: Parameterize this
+        int numPartitions = 2; // TODO: Shouldn't be hardcoded
+
+        dataDS = dataDS.withColumn(rowIndex, functions.monotonicallyIncreasingId());
+        tempDS = dataDS.select(columnName, rowIndex);
+        dataDS = dataDS.withColumnRenamed(columnName, deleteCol);
+
+        JavaPairRDD<String, Long> anonRdd = tempDS.javaRDD()
+                .mapToPair((PairFunction<Row, String, Long>) row ->
+                    {
+                        if (row.isNullAt(0))
+                            return new Tuple2<>(" ", row.getLong(1));
+                        else
+                            return new Tuple2<>(row.getString(0), row.getLong(1));
+                    })
+                .repartition(numPartitions);
+
+        PIIAnonymization anonymization = new PIIAnonymization();
+        JavaPairRDD<String, Long> resultRdd = anonymization.anonymizeData(anonRdd);
+
+        resultDS = spark.createDataset(resultRdd.collect(), Encoders.tuple(Encoders.STRING(), Encoders.LONG()));
+
+        anonymizedDS = dataDS.join(resultDS, rowIndex).drop(deleteCol).drop(rowIndex);
+
+        anonymizedDS.write(); // TODO: Update this
     }
 }
--- src/main/java/PIIAnonymization.java
+++ src/main/java/PIIAnonymization.java
@@ -1,6 +1,16 @@
+import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import scala.Tuple2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
 public class PIIAnonymization
@@ -9,8 +19,61 @@ public class PIIAnonymization
     private StanfordCoreNLP pipeline;
     private Annotation annotation;
 
-    public void anonymizeData()
+    public PIIAnonymization()
     {
+        this.props = new Properties();
+        this.props.put("annotators", "tokenize, ssplit, ner, regexner");
+    }
 
+    public JavaPairRDD<String, Long> anonymizeData(JavaPairRDD<String, Long> anonRdd)
+    {
+        anonRdd = anonRdd.mapPartitionsToPair((PairFlatMapFunction<Iterator<Tuple2<String,Long>>, String, Long>) partition ->
+        {
+            List<Tuple2<String, Long>> list = new ArrayList<>();
+
+            pipeline = new StanfordCoreNLP(props);
+
+            while (partition.hasNext())
+            {
+                Tuple2<String, Long> tuple = partition.next();
+                Tuple2<String, Long> result;
+
+                if (tuple._1.equals(" "))
+                    result = new Tuple2<>(tuple._1, tuple._2);
+                else {
+                    String line = classifyText(tuple._1, pipeline);
+                    result = new Tuple2<>(line, tuple._2);
+                }
+
+                list.add(result);
+            }
+
+            return list;
+        });
+        return anonRdd;
+    }
+
+    private String classifyText(String line, StanfordCoreNLP pipeline)
+    {
+        // Create an empty Annotation
+        annotation = new Annotation(line);
+
+        // Run all annotators on this text
+        pipeline.annotate(annotation);
+
+        // These are all the sentences in this document
+        // A CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence: sentences)
+        {
+            // Traversing the words in the current sentence
+            // A CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class))
+            {
+
+            }
+        }
+
+        return line;
     }
 }
