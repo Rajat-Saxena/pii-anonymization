46b859cc409c9c24accbd0486f4841e85725d43d
Rajat Saxena <rajatsaxena93@gmail.com>
2019-02-23 14:16:10 +0530

    Code updates for unit testing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.*;
@@ -9,47 +10,65 @@ public class Main
     private static Dataset<Row> tempDS;
     private static Dataset<Row> anonymizedDS;
     private static Dataset<Tuple2<String, Long>> resultDS;
+    private static Logger logger = Logger.getLogger(Main.class);
+    private static PIIAnonymization anonymization;
 
     public static void main(String args[])
     {
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("PII Anonymization")
-                .master("local")
-                .getOrCreate();
+        if (args.length < 3)
+        {
+            logger.error("Incorrect number of arguments.");
+        }
+        else
+        {
+            anonymization = new PIIAnonymization();
+            String sourceFilePath = args[0];
+            String targetDirectory = args[1];
+            String targetFilePath = targetDirectory + "/result.csv";
 
-        dataDS = spark.read()
-                .format("csv")
-                .option("header", "true")
-                .load(" ");
-        // TODO: Update the above
+            SparkSession spark = SparkSession
+                    .builder()
+                    .appName("PII Anonymization")
+                    .master("local")
+                    .getOrCreate();
 
-        String deleteCol = "deleteCol";
-        String rowIndex = "rowIndex";
-        String columnName = "desc"; // TODO: Parameterize this
-        int numPartitions = 2; // TODO: Shouldn't be hardcoded
+            logger.info("Reading source file from: " + sourceFilePath);
+            dataDS = spark.read()
+                    .format("csv")
+                    .option("header", "true")
+                    .load(sourceFilePath);
 
-        dataDS = dataDS.withColumn(rowIndex, functions.monotonicallyIncreasingId());
-        tempDS = dataDS.select(columnName, rowIndex);
-        dataDS = dataDS.withColumnRenamed(columnName, deleteCol);
+            String deleteCol = "deleteCol";
+            String rowIndex = "rowIndex";
+            String columnName = args[2];
+            int numPartitions = 2; // Change as per Spark configuration.
 
-        JavaPairRDD<String, Long> anonRdd = tempDS.javaRDD()
-                .mapToPair((PairFunction<Row, String, Long>) row ->
+            dataDS = dataDS.withColumn(rowIndex, functions.monotonicallyIncreasingId());
+            tempDS = dataDS.select(columnName, rowIndex);
+            dataDS = dataDS.withColumnRenamed(columnName, deleteCol);
+
+            JavaPairRDD<String, Long> anonRdd = tempDS.javaRDD()
+                    .mapToPair((PairFunction<Row, String, Long>) row ->
                     {
                         if (row.isNullAt(0))
                             return new Tuple2<>(" ", row.getLong(1));
                         else
                             return new Tuple2<>(row.getString(0), row.getLong(1));
                     })
-                .repartition(numPartitions);
+                    .repartition(numPartitions);
 
-        PIIAnonymization anonymization = new PIIAnonymization();
-        JavaPairRDD<String, Long> resultRdd = anonymization.anonymizeData(anonRdd);
+            logger.info("Starting anonymization.");
+            Long startTime = System.currentTimeMillis();
+            JavaPairRDD<String, Long> resultRdd = anonymization.anonymizeData(anonRdd);
 
-        resultDS = spark.createDataset(resultRdd.collect(), Encoders.tuple(Encoders.STRING(), Encoders.LONG()));
+            resultDS = spark.createDataset(resultRdd.collect(), Encoders.tuple(Encoders.STRING(), Encoders.LONG()));
 
-        anonymizedDS = dataDS.join(resultDS, rowIndex).drop(deleteCol).drop(rowIndex);
+            anonymizedDS = dataDS.join(resultDS, rowIndex).drop(deleteCol).drop(rowIndex);
 
-        anonymizedDS.write(); // TODO: Update this
+            logger.info("Anonymization complete. Took " + (System.currentTimeMillis() - startTime) + " (ms).");
+            logger.info("Writing data to target file: " + targetFilePath);
+
+            anonymizedDS.write().csv(targetFilePath);
+        }
     }
 }
--- src/main/java/PIIAnonymization.java
+++ src/main/java/PIIAnonymization.java
@@ -1,4 +1,3 @@
-import edu.stanford.nlp.ling.CoreAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -12,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class PIIAnonymization
 {
@@ -41,22 +41,23 @@ public class PIIAnonymization
                 if (tuple._1.equals(" "))
                     result = new Tuple2<>(tuple._1, tuple._2);
                 else {
-                    String line = classifyText(tuple._1, pipeline);
-                    result = new Tuple2<>(line, tuple._2);
+                    String text = classifyText(tuple._1, pipeline);
+                    result = new Tuple2<>(text, tuple._2);
                 }
 
                 list.add(result);
             }
 
-            return list;
+            return list.iterator();
         });
+
         return anonRdd;
     }
 
-    private String classifyText(String line, StanfordCoreNLP pipeline)
+    private String classifyText(String text, StanfordCoreNLP pipeline)
     {
         // Create an empty Annotation
-        annotation = new Annotation(line);
+        annotation = new Annotation(text);
 
         // Run all annotators on this text
         pipeline.annotate(annotation);
@@ -70,10 +71,20 @@ public class PIIAnonymization
             // A CoreLabel is a CoreMap with additional token-specific methods
             for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class))
             {
-
+                if (token.ner().equalsIgnoreCase("PERSON") ||
+                        token.ner().equalsIgnoreCase("LOCATION") ||
+                        token.ner().equalsIgnoreCase("ORGANIZATION") ||
+                        token.ner().equalsIgnoreCase("EMAIL") ||
+                        token.ner().equalsIgnoreCase("CITY") ||
+                        token.ner().equalsIgnoreCase("STATE_OR_PROVINCE") ||
+                        token.ner().equalsIgnoreCase("RELIGION"))
+                {
+                    text = text.replaceAll(Pattern.quote("\\b" + token.word() + "\\b"),
+                            "ANONYMIZED VALUE");
+                }
             }
         }
 
-        return line;
+        return text;
     }
 }
